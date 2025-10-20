3b10aa196cec181fd2cd7499ea52c9aff7720d34
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2025-10-20 16:22:09 +0200

    Added the dictionary content to the dictionary and a background

--- english-tale/Assets/Luckies/Scripts/UI/UIManager.cs
+++ english-tale/Assets/Luckies/Scripts/UI/UIManager.cs
@@ -1,14 +1,33 @@
+using System.Collections.Generic;
+using NUnit.Framework.Internal.Execution;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject dictionary;
-    private bool dictToggle = true;
+    public GameObject Dictionary;
+    public TMP_Text DictionaryText;
+    private bool DictToggle = true;
+
+    private List<WordManager.WordItem> WordsDb;
+    private string DictionaryDisplay = "";
+
+    private void Awake()
+    {
+        WordsDb = WordManager.Instance.getAllWords();
+
+        for (int i = 0; i < WordsDb.Count; i++)
+        {
+            DictionaryDisplay += WordsDb[i].eng_word + " - " + WordsDb[i].slo_word + "\n";
+        }
+        
+        DictionaryText.text = DictionaryDisplay;
+    }
 
     public void OnDictionaryToggle()
     {
-        dictionary.SetActive(dictToggle);
-        dictToggle = !dictToggle;
+        Dictionary.SetActive(DictToggle);
+        DictToggle = !DictToggle;
     }
 
 }
