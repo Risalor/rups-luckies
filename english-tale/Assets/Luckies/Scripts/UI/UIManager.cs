ae595f6018d3cd95792e10fff535fdc28ea4f0ea
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2025-10-26 22:55:26 +0100

    Dictionary animation

--- english-tale/Assets/Luckies/Scripts/UI/UIManager.cs
+++ english-tale/Assets/Luckies/Scripts/UI/UIManager.cs
@@ -6,15 +6,35 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public GameObject Dictionary;
+    private Animator anim = null;
     private bool DictToggle = true;
     private void Awake()
     {
+        anim = Dictionary.GetComponent<Animator>();
 
+        if (anim == null)
+        {
+            Debug.LogError("No Animator component found on Dictionary GameObject!");
+        }
     }
 
     public void OnDictionaryToggle()
     {
-        Dictionary.SetActive(DictToggle);
+        if(anim == null)
+        {
+            anim = Dictionary.GetComponent<Animator>();
+        }
+
+        if(DictToggle)
+        {
+            anim.SetBool("SlideIn", true);
+            anim.SetBool("SlideOut", false);
+        } else
+        {
+            anim.SetBool("SlideIn", false);
+            anim.SetBool("SlideOut", true);
+        }
+
         DictToggle = !DictToggle;
     }
 
