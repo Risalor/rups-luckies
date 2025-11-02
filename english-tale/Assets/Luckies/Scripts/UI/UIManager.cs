a1cd104dd521eed47017f59a43c9afc32f34872e
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2025-11-02 17:14:51 +0100

    Added some audio

--- english-tale/Assets/Luckies/Scripts/UI/UIManager.cs
+++ english-tale/Assets/Luckies/Scripts/UI/UIManager.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public GameObject Dictionary;
+    public AudioClip OpenSound;
+    private AudioSource audioSource;
     private Animator anim = null;
     private bool DictToggle = true;
     private void Awake()
     {
         anim = Dictionary.GetComponent<Animator>();
+        audioSource = Dictionary.GetComponent<AudioSource>();
 
         if (anim == null)
         {
@@ -20,20 +23,29 @@ public class UIManager : MonoBehaviour
 
     public void OnDictionaryToggle()
     {
-        if(anim == null)
+        if (anim == null)
         {
             anim = Dictionary.GetComponent<Animator>();
         }
+        
+        if(audioSource == null)
+        {
+            audioSource = Dictionary.GetComponent<AudioSource>();
+        }
 
-        if(DictToggle)
+        if (DictToggle)
         {
             anim.SetBool("SlideIn", true);
             anim.SetBool("SlideOut", false);
-        } else
+        }
+        else
         {
             anim.SetBool("SlideIn", false);
             anim.SetBool("SlideOut", true);
         }
+
+        audioSource.clip = OpenSound;
+        audioSource.Play();
 
         DictToggle = !DictToggle;
     }
