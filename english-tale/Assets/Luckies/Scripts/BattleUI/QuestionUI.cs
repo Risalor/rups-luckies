4519f639e46f050480fcb2a244301159761db7d5
Aljosa Vertot <aljosa.vertot@gmail.com>
2025-11-06 02:01:36 +0100

    First build

--- english-tale/Assets/Luckies/Scripts/BattleUI/QuestionUI.cs
+++ english-tale/Assets/Luckies/Scripts/BattleUI/QuestionUI.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
 using Debug = UnityEngine.Debug;
 using Random = UnityEngine.Random;
 
